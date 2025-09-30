59581476dd7ad72f2099600a0f83faaef69225ec
Sayuri Cero <Hisaalqaoud@gmail.com>
2025-09-30 03:42:17 +0300

    Latest update

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
-import { Redirect } from 'expo-router';
+//import { Redirect } from 'expo-router';
 
-const MODE: 'auth' | 'app' = 'auth';       // change to 'app' to test tabs
-const ROLE: 'user' | 'organizer' = 'user'; // used only when MODE === 'app'
+import OrgnaizerHomeScreen from "@/components/orgnaizer/OrgnaizerHomeScreen";
+
+//const MODE: 'auth' | 'app' = 'auth';       // change to 'app' to test tabs
+//const ROLE: 'user' | 'organizer' = 'user'; // used only when MODE === 'app'
 
 export default function Index() {
-  if (MODE === 'auth') return <Redirect href="/(auth)/signup" />; // or "/(auth)/signup"
-  return ROLE === 'organizer' ? <Redirect href="/(organizer)" /> : <Redirect href="/(tabs)" />;
+  //if (MODE === 'auth') return <Redirect href="/(auth)/signup" />; // or "/(auth)/signup"
+  //return ROLE === 'organizer' ? <Redirect href="/(organizer)" /> : <Redirect href="/(tabs)" />;
+  return <OrgnaizerHomeScreen />
 }
