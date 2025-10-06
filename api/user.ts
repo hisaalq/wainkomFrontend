7a73854bc19fbb6598ae0ef9cbcdefd21ba4928b
Sayuri Cero <Hisaalqaoud@gmail.com>
2025-10-06 18:18:16 +0300

    save

--- api/user.ts
+++ api/user.ts
@@ -1,4 +1,4 @@
-import { UserInfo, UserInfoMore } from "@/types";
+import { UserInfoMore } from "@/types";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import instance from ".";
 
@@ -12,26 +12,14 @@ export const getuser = async () => {
   return resp.data; 
 };
 
-const updateUser = async (userInfo: Partial<UserInfoMore>) => {
+export const updateUser = async (userInfo: Partial<UserInfoMore>) => {
   const { data } = await instance.put<UserInfoMore>("/updateprofile", userInfo);
   return data;
 };
-
-  const getUserInfo = async () => {
-    try{
-    const { data } = await instance.get<UserInfo[]>("/users");
-    return data;
-    } catch (error) {
-      console.error("Error getting users:", error);
-      throw error;
-    }
-  };
   
 
 export const logout = async () => {
   await AsyncStorage.removeItem("token");
 };
 
-
-  export { getUserInfo, updateUser };
 
